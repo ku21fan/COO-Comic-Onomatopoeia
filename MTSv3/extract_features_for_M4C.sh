9aef41dbbd17656736e780eda949e7cc8d962960
Jeonghun Baek <ku21fang@gmail.com>
2022-07-07 17:41:46 +0900

    Update extract_features_for_M4C.sh

--- MTSv3/extract_features_for_M4C.sh
+++ MTSv3/extract_features_for_M4C.sh
@@ -5,5 +5,3 @@ CUDA_VISIBLE_DEVICES=0 python extract_features_vmb.py --config_file configs/best
 CUDA_VISIBLE_DEVICES=0 python extract_features_vmb.py --config_file configs/best_M4C.yaml --image_dir ../COO-data/MTSv3_data/val_images/ --output_folder ../COO-data/M4C_feature/emb/val_gt_emb/ --use_gt
 CUDA_VISIBLE_DEVICES=0 python extract_features_vmb.py --config_file configs/best_M4C.yaml --image_dir ../COO-data/MTSv3_data/test_images/ --output_folder ../COO-data/M4C_feature/emb/test_gt_emb/ --use_gt
 
-
-#asdf
