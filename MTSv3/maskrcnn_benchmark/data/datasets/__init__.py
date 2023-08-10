a3e5ff394c62fc68edbac4af081a0c0ad23bd40b
Jeonghun Baek <ku21fang@gmail.com>
2023-08-10 08:47:01 +0000

    fix related to data folder

---
+++ MTSv3/maskrcnn_benchmark/data/datasets/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+from .coco import COCODataset
+from .concat_dataset import ConcatDataset, MixDataset
+from .icdar import IcdarDataset
+from .scut import ScutDataset
+from .synthtext import SynthtextDataset
+from .total_text import TotaltextDataset
+
+__all__ = [
+    "COCODataset",
+    "ConcatDataset",
+    "IcdarDataset",
+    "SynthtextDataset",
+    "MixDataset",
+    "ScutDataset",
+    "TotaltextDataset",
+]
